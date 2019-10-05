299cced10dcc570452c11d0df4c12a24c52ccad2
Wim Fournier <wifournier@ebay.com>
2019-10-05 15:37:43 +0200

    added timescaledb:Add

--- pkg/database/timescaledb.go
+++ pkg/database/timescaledb.go
@@ -2,18 +2,19 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/hsmade/OSM-ARDF/pkg/measurement"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type TimescaleDB struct {
-	Host         string
-	Port         uint16
-	Username     string
-	Password     string
-	DatabaseName string
-	connection   *pgxpool.Pool
+	Host           string
+	Port           uint16
+	Username       string
+	Password       string
+	DatabaseName   string
+	connectionPool *pgxpool.Pool
 }
 
 func (d *TimescaleDB) Connect() error {
@@ -25,11 +26,32 @@ func (d *TimescaleDB) Connect() error {
 		d.DatabaseName,
 	)
 	pool, err := pgxpool.Connect(context.Background(), url)
-	d.connection = pool
+	d.connectionPool = pool
 	return err
 }
 
 func (d *TimescaleDB) Add(m *measurement.Measurement) error {
+	conn, err := d.connectionPool.Acquire(context.Background())
+	if err != nil {
+		return err
+	}
 
+	defer conn.Release()
+
+	result, err := conn.Exec(context.Background(), "insert into doppler(time, name,lon,lat, bearing) values(%s, %s, %s, %s) ",
+		m.Timestamp,
+		m.Station,
+		m.Longitude,
+		m.Latitude,
+		m.Bearing,
+		)
+
+	if err != nil {
+		return err
+	}
+
+	if result.RowsAffected() != 1 {
+		return errors.New(fmt.Sprintf("Insert result in %d amount of rows, instead of 1", result.RowsAffected()))
+	}
 	return nil
 }
