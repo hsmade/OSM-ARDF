fe1f0909b2ecf36075518df272689e661e1937de
Wim Fournier <wifournier@ebay.com>
2019-10-15 23:42:01 +0200

    improve redirect readability

--- pkg/web/routes.go
+++ pkg/web/routes.go
@@ -7,5 +7,10 @@ import (
 
 func (s *server) routes() {
 	s.router.StaticFS("/app", web.Assets)
-	s.router.GET("/", func(c *gin.Context) {c.Request.URL.Path = "/app"; s.router.HandleContext(c)})
+	s.router.GET("/", s.redirectRoot)
+}
+
+func (s *server) redirectRoot(c *gin.Context) {
+	c.Request.URL.Path = "/app"
+	s.router.HandleContext(c)
 }
