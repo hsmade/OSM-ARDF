2e6485695f7a51e23c80616b08ece3a062cc0c98
Wim Fournier <wifournier@ebay.com>
2019-10-21 20:51:33 +0200

    merge the 2 init scripts for db

---
+++ deployments/timescaledb_init/init-gis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -xe
+#    CREATE TABLE doppler (time TIMESTAMPTZ NOT NULL DEFAULT now(), name TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL, bearing INT);
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE EXTENSION postgis;
+    CREATE TABLE doppler (time TIMESTAMPTZ NOT NULL DEFAULT now(), name TEXT NOT NULL, point GEOMETRY, bearing INT);
+    SELECT create_hypertable('doppler', 'time', chunk_time_interval => INTERVAL '1 minute');
+EOSQL
--- deployments/timescaledb_init/init.sh
+++ deployments/timescaledb_init/init.sh
@@ -8,6 +8,4 @@ done
 
 >&2 echo "Postgres is up - creating DB"
 
-PGPASSWORD=$POSTGRES_PASSWORD psql -h "timescaledb" -U "postgres" -c "CREATE EXTENSION postgis;"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h "timescaledb" -U "postgres" -c "create table doppler (time TIMESTAMPTZ not null default now(), name text not null, point GEOMETRY, bearing INT);"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h "timescaledb" -U "postgres" -c "SELECT create_hypertable('doppler', 'time', chunk_time_interval => interval '1 minute');"
+/init-git.sh
